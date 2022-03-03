fe9e9c14d773968e5eb63b9b0d9370ad9d11c824
H3ndr1k <hacongchieu@gmail.com>
2022-03-03 20:26:07 +0700

    Oke

--- server.js
+++ server.js
@@ -11,15 +11,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 2805;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
+var token = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0,10);
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ status : 'true',
+        message: ""+token });
 });
 
 // more routes for our API will happen here
